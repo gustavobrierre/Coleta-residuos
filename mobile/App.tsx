bc1ffd53b21f82962f7aff888858973d01e55354
Gustavo Brierre <gustavobrierre@gmail.com>
2020-08-03 12:27:47 -0300

    APP Finalizado

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { AppLoading } from "expo";
+import { StatusBar } from "react-native";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import Routes from "./src/routes";
+
+export default function App() {
+  const [fonstLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  if (!fonstLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
+  );
+}
